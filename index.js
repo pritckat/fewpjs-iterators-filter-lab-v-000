16b98fd63ea5d07be0bbfba14cc6edbfc54a8ecb
Katrina Pritchard <pritckat@gmail.com>
2021-01-14 18:03:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(arr, driver) {
+  return arr.filter(d => d.toLowerCase() == driver.toLowerCase())
+}
+
+function fuzzyMatch(arr, letters) {
+  return arr.filter(d => d.slice(0, letters.length) == letters)
+}
+
+function matchName(arr, driver) {
+  return arr.filter(d => d.name == driver)
+}
