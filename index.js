78c2325514ad6db2f574eae25fabdcf181d2d1b9
Katrina Pritchard <pritckat@gmail.com>
2021-01-14 17:58:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ function findMatching(arr, driver) {
 }
 
 function fuzzyMatch(arr, letters) {
-  return arr.filter(d => d.slice(d.length) == letters)
+  return arr.filter(d => d.slice(letters.length) == letters)
 }
